6e2a58a677f8ec72cc34c639e5fd130463e32be5
Khoi Hoang <khoii87@gmail.com>
2023-02-05 20:25:26 +0100

    add: ✨ disable react dev tools in production

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import AuthProvider from './context/auth-provider';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
 import './index.css';
+
+if (process.env.NODE_ENV === 'production') {
+	disableReactDevTools();
+}
 
 ReactDOM.render(
 	<React.StrictMode>
